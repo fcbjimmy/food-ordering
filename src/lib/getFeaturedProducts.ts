f5e6e9b2c1fae9860c1f903cca63329e72757156
fcbjimmy <jim.culers@gmail.com>
2023-09-11 19:20:05 +0800

    test

--- src/lib/getFeaturedProducts.ts
+++ src/lib/getFeaturedProducts.ts
@@ -1,25 +1,25 @@
-export const getFeaturedProducts = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/products?featured=yes`, {
-    cache: "no-store",
-  });
+// export const getFeaturedProducts = async () => {
+//   const res = await fetch(`${process.env.BASE_URL}/api/products?featured=yes`, {
+//     cache: "no-store",
+//   });
 
-  console.log(
-    "----------------------------------CONSOLE----------------------------------"
-  );
-  console.log(process.env.BASE_URL);
-  console.log(res);
+//   console.log(
+//     "----------------------------------CONSOLE----------------------------------"
+//   );
+//   console.log(process.env.BASE_URL);
+//   console.log(res);
 
-  if (!res.ok) return undefined;
+//   if (!res.ok) return undefined;
 
-  return res.json();
-};
+//   return res.json();
+// };
 
-export const getProducts = async () => {
-  const res = await fetch(`${process.env.BASE_URL}/api/products?featured=no`, {
-    cache: "no-store",
-  });
+// export const getProducts = async () => {
+//   const res = await fetch(`${process.env.BASE_URL}/api/products?featured=no`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) undefined;
+//   if (!res.ok) undefined;
 
-  return res.json();
-};
+//   return res.json();
+// };
--- src/lib/getProduct.ts
+++ src/lib/getProduct.ts
@@ -1,9 +1,9 @@
-export const getProduct = async (id: string) => {
-  const res = await fetch(`${process.env.BASE_URL}/api/products/${id}`, {
-    cache: "no-store",
-  });
+// export const getProduct = async (id: string) => {
+//   const res = await fetch(`${process.env.BASE_URL}/api/products/${id}`, {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) return undefined;
+//   if (!res.ok) return undefined;
 
-  return res.json();
-};
+//   return res.json();
+// };
